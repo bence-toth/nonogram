f103d50d1ccdf836f4953b0e02cc585da98d2508
Bence A. Tóth <tothab@gmail.com>
2023-09-05 11:06:23 +0200

    Some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,41 @@
-const App = () => <div></div>;
+import "./App.css";
+
+const puzzle: (string | null)[][] = [];
+
+const numberOfRows = 10;
+const numberOfColumns = 14;
+
+for (let i = 0; i < numberOfRows; i++) {
+  const row = [];
+  for (let j = 0; j < numberOfColumns; j++) {
+    if (Math.random() > 0.4) {
+      const randomColor = `#${Math.floor(Math.random() * 16777215).toString(
+        16
+      )}`;
+      row.push(randomColor);
+    } else {
+      row.push(null);
+    }
+  }
+  puzzle.push(row);
+}
+
+const App = () => (
+  <div className="board">
+    {puzzle.map((row, rowIndex) => (
+      <div className="row" key={rowIndex}>
+        {row.map((column, columnIndex) => (
+          <div
+            className="cell"
+            key={columnIndex}
+            {...(column !== null && { style: { backgroundColor: column } })}
+          >
+            {column}
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+);
 
 export default App;
